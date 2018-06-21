2a1b1f810898c541da46b47235e72cbf305a48da
aksenthilk <aksenthilk@gmail.com>
2018-06-22 02:07:24 +0530

    Changed Junit Lib

--- src/main/java/StepDefinitions/Steps.java
+++ src/main/java/StepDefinitions/Steps.java
@@ -3,6 +3,7 @@ package StepDefinitions;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -19,9 +20,8 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
 
-@SuppressWarnings("deprecation")
+
 public class Steps{
 	WebDriver driver;
 	 ExtentReports extent;
